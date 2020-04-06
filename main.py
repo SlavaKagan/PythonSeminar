6f3f20c024327b87b9aca4ac910fceff356baa93
Slava Kagan <slava15.92@gmail.com>
2020-04-07 00:51:57 +0300

    Organize the project

--- main.py
+++ main.py
@@ -51,37 +51,49 @@ if __name__ == '__main__':
     print(f'Sequential time Decryption result: {total_time_dec_seq} sec\n')
 
     "##################### Parallel Section - Fork #####################"
-    print('############## Parallel Section - Fork ###########')
-    print('Please Wait...')
-
     headers = ['Number of threads', 'fork join run time', 'Data the same as in sequential?']
-    with open("Results text files/encryption_fork_file.txt", "w") as file_encryption:
-        file_encryption.write("########### Encryption Results ###############:\n")
-        file_encryption.write(''.join(column.rjust(10) for column in headers))
+    print('############## Parallel Section ###########')
+    print("Note: Time results depends on the load on the computer.\n "
+          "There are many other processes and so there may be many delays")
+    print('############## Fork Implementation ###########')
+    print('--- Encrypting ---\nPlease Wait...')
+
+    with open("Results text files/encryption_time.txt", "w") as file_encryption:
+        file_encryption.write("########### Fork Encryption Time Results ###############:\n")
+        file_encryption.write("Note: Time results depends on the load on the computer.\n "
+                              "There are many other processes and so there may be many delays\n\n")
+        file_encryption.write('\t\t'.join(map(str, headers)))
         file_encryption.write("\n")
-        for num_threads in range(2, maximum_num_of_threads+1):
+        for num_threads in range(2, maximum_num_of_threads + 1):
+            print(f"Working on fork implement - encryption - number of threads:  {num_threads},", end=" ")
             des_para_fork = Parallel_Des_Fork(key, num_threads)
             start_enc_para_fork = time.time()
             encrypted_text_para_fork = des_para_fork.encrypt(text, num_threads)  # parallel fork encryption text with DES algorithm
             end_enc_para_fork = time.time()
             total_time_enc_para_fork = round(float(end_enc_para_fork) - float(start_enc_para_fork), 3)
-            file_encryption.write(f"{num_threads}\t\t{total_time_enc_para_fork}\t\t{encrypted_text_seq == encrypted_text_para_fork}")
+            print(f"\tTime: {total_time_enc_para_fork} sec")
+            file_encryption.write(f"\t{num_threads}\t\t\t\t\t{total_time_enc_para_fork} sec\t\t\t\t{encrypted_text_seq == encrypted_text_para_fork}")
             file_encryption.write("\n")
-        file_encryption.write(f"Run time without multi threading:\t{total_time_enc_seq}\n")
+    print("Finished Encrypting\n")
 
-    with open("Results text files/decryption_fork_file.txt", "w") as file_decryption:
-        file_decryption.write("########### Decryption Results ###############:\n")
-        file_decryption.write(''.join(column.rjust(10) for column in headers))
+    print('--- Decrypting ---\nPlease Wait...')
+    with open("Results text files/decryption_time.txt", "w") as file_decryption:
+        file_decryption.write("########### Fork Decryption Time Results ###############:\n")
+        file_decryption.write("Note: Time results depends on the load on the computer.\n "
+                              "There are many other processes and so there may be many delays\n\n")
+        file_decryption.write('\t\t'.join(map(str, headers)))
         file_decryption.write("\n")
-        for num_threads in range(2, maximum_num_of_threads+1):
+        for num_threads in range(2, maximum_num_of_threads + 1):
+            print(f"Working on fork implement - decryption - number of threads:  {num_threads},", end=" ")
             des_para_fork = Parallel_Des_Fork(key, num_threads)
             start_dec_para_fork = time.time()
-            decrypted_text_para_fork = des_para_fork.decrypt(text, num_threads)  # parallel fork decryption text with DES algorithm
+            decrypted_text_para_fork = des_para_fork.decrypt(encrypted_text_seq, num_threads)  # parallel fork decryption text with DES algorithm
             end_dec_para_fork = time.time()
             total_time_dec_para_fork = round(float(end_dec_para_fork) - float(start_dec_para_fork), 3)
-            file_decryption.write(f"{num_threads}\t\t{total_time_dec_para_fork}\t\t{decrypted_text_seq == decrypted_text_para_fork}")
+            print(f"\tTime: {total_time_dec_para_fork} sec")
+            file_decryption.write(f"\t{num_threads}\t\t\t\t\t{total_time_dec_para_fork} sec\t\t\t\t{decrypted_text_seq == decrypted_text_para_fork}")
             file_decryption.write("\n")
-        file_decryption.write(f"Run time without multi threading:\t{total_time_dec_seq}\n")
+    print("Finished Decrypting\n")
 
     "##################### Parallel Section - Pool #####################"
     print('############## Parallel Section - Pool ###########')
@@ -93,18 +105,19 @@ if __name__ == '__main__':
     end_enc_para_pool = time.time()
     total_time_enc_para_pool = round(float(end_enc_para_pool) - float(start_enc_para_pool), 3)
     print(f"Parallel time Encryption pool way result: {total_time_enc_para_pool} sec")
-
-    start_dec_para_fork = time.time()
-    decrypted_text_para_fork = des_para_fork.decrypt(encrypted_text_para_fork)  # parallel fork decryption ciphered text with DES algorithm
-    end_dec_para_fork = time.time()
-    total_time_dec_para_fork = round(float(end_dec_para_fork) - float(start_dec_para_fork), 3)
-    print(f'Parallel time Decryption fork way result: {total_time_dec_para_fork} sec')
+    with open("Results text files/encryption_time.txt", "a") as file_encryption:
+        file_encryption.write(f"Runtime Parallel pool way result: {total_time_enc_para_pool} sec\n")
+        file_encryption.write(f"Run time without multi threading: {total_time_enc_seq} sec")
 
     start_dec_para_pool = time.time()
     decrypted_text_para_pool = des_para_pool.decrypt(encrypted_text_para_pool)  # parallel pool decryption ciphered text with DES algorithm
     end_dec_para_pool = time.time()
     total_time_dec_para_pool = round(float(end_dec_para_pool) - float(start_dec_para_pool), 3)
-    print(f'Parallel time Decryption pool way result: {total_time_dec_para_pool} sec\n')
+    print(f'Parallel time Decryption pool way result: {total_time_dec_para_pool} sec')
+    with open("Results text files/decryption_time.txt", "a") as file_decryption:
+        file_decryption.write(f"Runtime Parallel pool way result: {total_time_dec_para_pool} sec\n")
+        file_decryption.write(f"Run time without multi threading: {total_time_dec_seq} sec")
+    print("\n--- Program finished running, now you can go to results in txt files ---")
 
     "##################### File Data Section #####################"
     with open("Results text files/data_results_fork.txt", "w") as file_fork:
